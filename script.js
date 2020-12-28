2ea8a1cec0a3859c52af3989815cd2037e38d2b0
beatarynkevic <beatainn@gmail.com>
2020-12-28 23:12:50 +0200

    Function declaration and expression

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
 'use strict';
 
-let hasDriversLicense = false;
+/*let hasDriversLicense = false;
 const passTest = true;
 
 if (passTest === true) {
     hasDriversLicense = true;
     console.log('yee');
-} 
+}*/
+
+function logger() {
+    console.log('My name is Jonas');
+}
+
+//  calling / running / invoking function
+logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `juice with ${apples} apples and ${oranges} oranges`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+
+
+//Function declaration
+function calcAge1(birthYear) { //parameter
+    return 2037 - birthYear;
+}
+
+console.log(calcAge1(1991)); //arguments
+
+
+
+
+//Function expression
+const calcAge2 = function (birthYear) { // anonymous function expression
+    return 2037 - birthYear;
+}
+
+console.log(calcAge2(1991));
